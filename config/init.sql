28a58bf84a50bb3fe3db1598004a9dea8f43e2e8
fufu_lola <62877954+fufulola@users.noreply.github.com>
2023-05-21 23:40:38 +0200

    Mise a jour Fînal
    La mise a jour du redu final
    
    Co-Authored-By: Ploudo <17126689+Ploudo@users.noreply.github.com>

--- config/init.sql
+++ config/init.sql
@@ -1,14 +1,14 @@
--- CrÃ©ation de la base de donnÃ©es
+-- Création de la base de données
 CREATE DATABASE wordpress;
 
--- Utilisation de la base de donnÃ©es
+-- Utilisation de la base de données
 USE wordpress;
 
--- CrÃ©ation d'un utilisateur pour la base de donnÃ©es
+-- Création d'un utilisateur pour la base de données
 CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
 
--- Attribution des privilÃ¨ges Ã  l'utilisateur
+-- Attribution des privilèges à l'utilisateur
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost';
 
--- Mise Ã  jour des privilÃ¨ges
+-- Mise à jour des privilèges
 FLUSH PRIVILEGES;
--- config/wp-config.php
+++ config/wp-config.php
@@ -1,62 +1,12 @@
 <?php
-/**
- * Configuration de base de WordPress.
- *
- * Ce fichier contient les configurations suivantes :
- *
- * * Les réglages MySQL
- * * Préfixe de table
- * * (Clés secrètes à voir si on rajoute)
- * * Langue utilisée
- * * ABSPATH
- *
- */
-
-// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. Dans notre cas c'est nous qu'avons choisi ** //
-/** Nom de la base de données de WordPress */
-define( 'DB_NAME', 'wordpress' );
-
-/** Utilisateur de la base de données MySQL */
-define( 'DB_USER', 'wordpress' );
-
-/** Mot de passe de la base de données MySQL */
-define( 'DB_PASSWORD', 'wordpress' );
-
-/** Adresse de l'hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
-
-/** Jeu de caractères de la base de données à utiliser lors de la création des tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
-
-/** Type de collation de la base de données. Ne changez pas ceci si en dout. */
-define( 'DB_COLLATE', '' );
-
-// Préfixe de table pour la base de données de WordPress.
-// Vous pouvez avoir plusieurs installations dans une seule base de données si vous donnez à chaque installation un préfixe unique.
-$table_prefix = 'wp_';
-
-// Clés secrètes
-// Changez ces clés secrètes si elles ont été mises en production.
-//define( 'AUTH_KEY',         'mettez votre propre phrase unique ici' );
-//define( 'SECURE_AUTH_KEY',  'mettez votre propre phrase unique ici' );
-//define( 'LOGGED_IN_KEY',    'mettez votre propre phrase unique ici' );
-//define( 'NONCE_KEY',        'mettez votre propre phrase unique ici' );
-//define( 'AUTH_SALT',        'mettez votre propre phrase unique ici' );
-//define( 'SECURE_AUTH_SALT', 'mettez votre propre phrase unique ici' );
-//define( 'LOGGED_IN_SALT',   'mettez votre propre phrase unique ici' );
-//define( 'NONCE_SALT',       'mettez votre propre phrase unique ici' );
-
-// Langue utilisée
-// Remplacez la langue par défaut (en_US) par votre langue préférée si vous êtes contre la langue universelle.
-define( 'WPLANG', 'fr_FR' );
-
-// Chemin absolu vers le répertoire de WordPress.
-if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );
-}
-
-// Désactiver les mises à jour automatiques pour WordPress.
-define( 'AUTOMATIC_UPDATER_DISABLED', true );
-
-// Désactiver la modification des fichiers dans l'interface d'administration de WordPress.
-define( 'DISALLOW_FILE_EDIT', true );
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'wordpress');
+define('DB_HOST', 'mysql');
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
+$table_prefix  = 'wp_';
+define('WP_DEBUG', false);
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
